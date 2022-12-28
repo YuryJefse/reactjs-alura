ee9b32d18c3c219d738966dbfffd5f1500f89345
009017L9 <yury.silva@sefaz.ce.gov.br>
2022-12-28 17:04:20 -0300

    Adicionando Sass e configurando os styles dos componentes

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Botao from "../Botao";
+import './style.scss';
 
 class Formulario extends React.Component {
     render() {
         return (
-            <form>
-                <div>
+            <form className="novaTarefa">
+                <div className="inputContainer">
                     <label htmlFor="tarefa" >Adicione a tarefa</label>
                     <input
                         type="text"
                         name="tarefa"
                     />
                 </div>
-                <div>
+                <div className="inputContainer">
                     <label>Tempo</label>
                     <input type="time"
                     step="1"/>
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,12 +1,14 @@
+import './style.scss';
+
 function Lista() {
     const tarefas = [{ tarefa: "Estudar React", tempo: "02:00:00" }, { tarefa: "Estudar JS", tempo: "01:00:00" }]
 
     return (
-        <aside>
+        <aside className='listaTarefas'>
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index}>
+                    <li className='item' key={index}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
