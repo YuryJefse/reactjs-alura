30df262cc365cab6bb60941fd971897789aec920
009017L9 <yury.silva@sefaz.ce.gov.br>
2023-01-02 20:57:21 -0300

    Aula de estilo com sass

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,14 +1,14 @@
-import './style.scss';
+import style from "./Lista.module.scss";
 
 function Lista() {
     const tarefas = [{ tarefa: "Estudar React", tempo: "02:00:00" }, { tarefa: "Estudar JS", tempo: "01:00:00" }]
 
     return (
-        <aside className='listaTarefas'>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li className='item' key={index}>
+                    <li className={style.item} key={index}>
                         <h3>{item.tarefa}</h3>
                         <span>{item.tempo}</span>
                     </li>
